49fef6bdd49e1130ef491918c46ac369c78b7d39
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:29:51 +0700

    Update

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo fire("x\\b_b_code", [$content], (object) ['type' => 'BBCode']);
 
